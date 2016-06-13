9fba280221a222c9762cfe77326030a8267a9c00
Paolo D'Incau <paolo.dincau@xpeppers.com>
2016-06-13 19:27:29 +0200

    add logs

--- src/main/java/infrastructure/InMemoryBookRepository.java
+++ src/main/java/infrastructure/InMemoryBookRepository.java
@@ -6,24 +6,35 @@ import domain.Book;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class InMemoryBookRepository implements BookRepository {
+
+    static final Logger LOG = LoggerFactory.getLogger(InMemoryBookRepository.class);
 
     List<Book> books;
     Integer sequence;
 
     public InMemoryBookRepository() {
         this.books = new ArrayList<>();
-        this.sequence = 0;
+        this.sequence = 1;
     }
 
     @Override
     public Book find(Integer bookId) {
+        LOG.info("Find book with id: {}", bookId);
         return books.stream().filter(b -> b.hasId(bookId)).findFirst().orElse(new Book());
     }
 
     @Override
     public void save(Book book) {
-        book.setId(sequence);
+        LOG.info("Save book using id: {}", sequence);
+        book.setId(nextId());
         books.add(book);
+    }
+
+    Integer nextId() {
+        return sequence++;
     }
 }

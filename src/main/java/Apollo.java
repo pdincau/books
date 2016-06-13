984b3de2b05f17f84cb51125cd54c42b088d08e5
Paolo D'Incau <paolo.dincau@xpeppers.com>
2016-06-13 13:29:47 +0200

    Add first route to rate a book

--- src/main/java/Apollo.java
+++ src/main/java/Apollo.java
@@ -1,9 +1,18 @@
+import actions.RateBook;
+import actions.BooksActiondHandler;
 import com.spotify.apollo.Environment;
+import com.spotify.apollo.RequestContext;
+import com.spotify.apollo.Response;
+import com.spotify.apollo.Status;
 import com.spotify.apollo.httpservice.HttpService;
 import com.spotify.apollo.httpservice.LoadingException;
 import com.spotify.apollo.route.Route;
+import okio.ByteString;
+
 
 public class Apollo {
+
+    private static BooksActiondHandler actiondHandler = new BooksActiondHandler();
 
     public static void main(String[] args) throws LoadingException {
         HttpService.boot(Apollo::init, "apollo", args);
@@ -11,7 +20,11 @@ public class Apollo {
 
     static void init(Environment environment) {
         environment.routingEngine()
-                .registerAutoRoute(Route.sync("GET", "/", context -> "hello world"))
-                .registerAutoRoute(Route.sync("GET", "/hello", context -> "bye"));
+                .registerAutoRoute(Route.sync("GET", "/books/1/rate", Apollo::rateBook));
+    }
+
+    private static Response<ByteString> rateBook(RequestContext context)  {
+        actiondHandler.handle(new RateBook("a book id", "a description", 5, "a user id"));
+        return Response.forStatus(Status.CREATED);
     }
 }
---
+++ src/main/java/actions/BooksActiondHandler.java
@@ -0,0 +1,7 @@
+package actions;
+
+public class BooksActiondHandler {
+
+    public void handle(RateBook action) {
+    }
+}

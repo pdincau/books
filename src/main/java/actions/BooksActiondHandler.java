d540bd581eca3dd7ebd232691ac8579bc203be8c
Paolo D'Incau <paolo.dincau@xpeppers.com>
2016-06-13 19:39:56 +0200

    add logs

--- src/main/java/actions/BooksActiondHandler.java
+++ src/main/java/actions/BooksActiondHandler.java
@@ -2,8 +2,12 @@ package actions;
 
 import domain.Book;
 import infrastructure.InMemoryBookRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class BooksActiondHandler {
+
+    static final Logger LOG = LoggerFactory.getLogger(BooksActiondHandler.class);
 
     BookRepository repository;
 
@@ -16,10 +20,12 @@ public class BooksActiondHandler {
     }
 
     public void handle(AddBook action) {
+        LOG.info("Handle command: {}", action);
         repository.save(action.getBook());
     }
 
     public void handle(RateBook action) {
+        LOG.info("Handle command: {}", action);
         Book book = repository.find(action.getBookId());
         book.rate(action.getRating());
     }

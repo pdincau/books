2e290b24d33954d720f29f199b08cd7af780bd5f
Andrea Bertagnolli <andrea.bertagnolli@paros.srl>
2016-10-28 15:40:36 +0200

    Remove not-abstract class

--- src/main/java/infrastructure/queue/Producer.java
+++ src/main/java/infrastructure/queue/Producer.java
@@ -1,11 +1,23 @@
 package infrastructure.queue;
 
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
 import java.io.IOException;
 
-public class Producer extends EndPoint {
+public class Producer {
 
-    public Producer(String endPointName) throws IOException {
-        super(endPointName);
+    private Channel channel;
+    private String endPointName;
+
+    public Producer(String endpointName) throws IOException {
+        this.endPointName = endpointName;
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        channel = connection.createChannel();
+        channel.exchangeDeclare(endpointName, "direct");
     }
 
     public void sendMessage(String object, String topic) throws IOException {
